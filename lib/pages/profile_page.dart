e79bb614558fd50d3faea29ab51e19c31a62fa6f
jack dunne <jackzoo123@outlook.com>
2022-06-25 01:29:24 +0100

    added scrolling image view to profile page using swiper plugin

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_new
+
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
@@ -13,18 +15,27 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.amber[600],
-          body: Swiper(
-            itemBuilder: (BuildContext context, int index) {
-              return new Image.network(
-                "http://via.placeholder.com/288x188",
-                fit: BoxFit.fill,
-              );
-            },
-            itemCount: 10,
-            viewportFraction: 0.8,
-            scale: 0.9,
-          )),
+        body: Column(
+          children: [
+            Container(
+                child: new Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return new Image.network(
+                      "http://via.placeholder.com/288x188",
+                      fit: BoxFit.fill,
+                    );
+                  },
+                  itemCount: 10,
+                  viewportFraction: 0.8,
+                  scale: 0.9,
+                ),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                height: MediaQuery.of(context).size.height * 0.5,
+                width: MediaQuery.of(context).size.width * 0.95),
+          ],
+        ),
+      ),
     );
   }
 }
