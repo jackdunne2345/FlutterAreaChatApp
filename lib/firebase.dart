06383cef707aa66ce31ccd9bcf7dfe45506946c0
jack dunne <jackzoo123@outlook.com>
2022-07-15 16:03:47 +0100

    added default bio and usser data manipulation function for signup and eddit

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Duration get loginTime => Duration(milliseconds: 2250);
 Future<String?> logIn(String name, String password) async {
@@ -17,8 +18,13 @@ Future<String?> logIn(String name, String password) async {
 
 Future<String?> signUp(String name, String password) async {
   try {
-    await FirebaseAuth.instance
+    UserCredential create = await FirebaseAuth.instance
         .createUserWithEmailAndPassword(email: name, password: password);
+    User user = create.user!;
+    //set user data to default data when account is made
+    String defBio = "Hi! I'm new :)";
+    giveData(defBio, user.uid);
+
     return Future.delayed(loginTime).then((_) async {
       //this null retunr means there is no errors and will laucnh app
       return null;
@@ -29,4 +35,13 @@ Future<String?> signUp(String name, String password) async {
       return e.message;
     });
   }
+}
+
+//this object a userData object referning to the "userCollection" colelction on my firestore
+
+final CollectionReference userData =
+    FirebaseFirestore.instance.collection('userCollection');
+
+Future giveData(String bio, String uid) async {
+  return await userData.doc(uid).set({'bio': bio});
 }
